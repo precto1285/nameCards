41adf289f06ead2c3022b7cdf519e5681da1c46a
Philippe Recto <precto1285@gmal.com>
2019-02-27 15:14:42 -0800

    updated form

--- namecards/src/Components/Form.js
+++ namecards/src/Components/Form.js
@@ -3,16 +3,23 @@ import React from 'react';
 const Form = () => {
   return (
     <div className="container my-5 py-5 bg-secondary">
-      <h2>Enter An Employee:</h2>
-      <hr />
-      <form>
-        <div className="form-group"><label for="nameInput">Name:</label><input id="nameInput" type="text" class="form-control"></input></div>
-        <div className="form-group"><label for="titleInput">Title:</label><input id="titleInput" type="text" class="form-control"></input></div>
-        <div className="form-group"><label for="departmentInput">Department:</label><input id="departmentInput" type="text" class="form-control"></input></div>
-        <div className="form-group"><label for="officeInput">Office:</label><input id="officeInput" type="text" className="form-control"></input></div>
-        <hr />
-        <button className="btn btn-danger form-control">Submit</button>
-      </form>
+      <div className="card bg-secondary">
+        <div className="card-header">
+          <h2>Enter An Employee:</h2>
+        </div>
+        <form>
+          <div className="card-body">
+            <div className="form-group"><label for="nameInput">Name:</label><input id="nameInput" type="text" class="form-control"></input></div>
+            <div className="form-group"><label for="titleInput">Title:</label><input id="titleInput" type="text" class="form-control"></input></div>
+            <div className="form-group"><label for="departmentInput">Department:</label><input id="departmentInput" type="text" class="form-control"></input></div>
+            <div className="form-group"><label for="officeInput">Office:</label><input id="officeInput" type="text" className="form-control"></input></div>
+          </div>
+          <div className="card-footer">
+            <button className="btn btn-danger form-control">Submit</button>
+          </div>
+        </form>
+
+      </div>
     </div>
   )
 }
