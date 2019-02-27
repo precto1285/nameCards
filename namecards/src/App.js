fa1c1fe17292083296e51c2fddb63f96a51ca5dd
Philippe Recto <precto1285@gmal.com>
2019-02-27 12:36:48 -0800

    creeated components

--- namecards/src/App.js
+++ namecards/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import CardOne from './Components/CardOne';
+import CardTwo from './Components/CardTwo';
+import CardThree from './Components/CardThree';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello World!</h1>
+        <CardOne />
+        <CardTwo />
+        <CardThree />
       </div>
     );
   }
---
+++ namecards/src/Components/CardOne.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CardOne extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Card One</h2>
+      </div>
+    );
+  }
+}
+
+export default CardOne;
