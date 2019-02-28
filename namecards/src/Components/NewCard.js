df031ed52b638fe320240dcf835103683a9aaa82
Philippe Recto <precto1285@gmal.com>
2019-02-27 19:06:01 -0800

    NewCard changed to class

--- namecards/src/Components/NewCard.js
+++ namecards/src/Components/NewCard.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const NewCard = () => {
-  return (
-    <div className="container">
-      <div className="card">
-        <div className="card-header text-center">
-          <h2>New Person:</h2>
+class NewCard extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="card">
+          <div className="card-header text-center">
+            <h2>New Person:</h2>
+          </div>
+          <div className="card-body">
+            <p>Name: </p>
+            <p>Title: </p>
+            <p>Department: </p>
+            <p>Office: </p>
+          </div>
+          <div className="card-footer"></div>
         </div>
-        <div className="card-body">
-          <p>Name: </p>
-          <p>Title: </p>
-          <p>Department: </p>
-          <p>Office: </p>
-        </div>
-        <div className="card-footer"></div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default NewCard;
