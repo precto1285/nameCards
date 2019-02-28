1dbc22c53be72701ab9d14b39cead9919a329cd9
Philippe Recto <precto1285@gmal.com>
2019-02-27 16:29:07 -0800

    added images

--- namecards/src/App.js
+++ namecards/src/App.js
@@ -5,6 +5,7 @@ import CardTwo from './Components/CardTwo';
 import CardThree from './Components/CardThree';
 import Footer from './Components/Footer';
 import Form from './Components/Form';
+import Image from './Components/Image';
 class App extends Component {
   constructor(prop) {
     super(prop);
@@ -36,6 +37,7 @@ class App extends Component {
     }
   }
 
+
   render() {
     return (
       <div className="App">
@@ -45,23 +47,34 @@ class App extends Component {
           </div>
           <div className="row">
             <div className="col-lg-12">
-              <Form />
+              <Form onSubmit="Hello World!" />
             </div>
           </div>
           <div className="row">
             <div className="col-md-4 px-5">
               <CardOne
-                cOneName={this.state.cOne[0].name} cOneTitle={this.state.cOne[0].title} cOneDepartment={this.state.cOne[0].department} cOneOffice={this.state.cOne[0].office} />
+                cOneName={this.state.cOne[0].name}
+                cOneTitle={this.state.cOne[0].title}
+                cOneDepartment={this.state.cOne[0].department}
+                cOneOffice={this.state.cOne[0].office} />
             </div>
             <div className="col-md-4 px-5">
               <CardTwo
-                cTwoName={this.state.cTwo[0].name} cTwoTitle={this.state.cTwo[0].title} cTwoDepartment={this.state.cTwo[0].department} cTwoOffice={this.state.cTwo[0].office} />
+                cTwoName={this.state.cTwo[0].name}
+                cTwoTitle={this.state.cTwo[0].title}
+                cTwoDepartment={this.state.cTwo[0].department}
+                cTwoOffice={this.state.cTwo[0].office} />
             </div>
             <div className="col-md-4 px-5">
               <CardThree
-                cThreeName={this.state.cThree[0].name} cThreeTitle={this.state.cThree[0].title}
-                cThreeDepartment={this.state.cThree[0].department} cThreeOffice={this.state.cThree[0].office} />
+                cThreeName={this.state.cThree[0].name}
+                cThreeTitle={this.state.cThree[0].title}
+                cThreeDepartment={this.state.cThree[0].department}
+                cThreeOffice={this.state.cThree[0].office} />
             </div>
+          </div>
+          <div className="row">
+            <Image />
           </div>
           <div className="row my-5">
             <div className="col-md-12">
--- namecards/src/Components/Form.js
+++ namecards/src/Components/Form.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Form = () => {
+
   return (
     <div className="container my-5 py-5 bg-secondary">
       <div className="card bg-secondary">
@@ -9,10 +10,22 @@ const Form = () => {
         </div>
         <form>
           <div className="card-body">
-            <div className="form-group"><label for="nameInput">Name:</label><input id="nameInput" type="text" class="form-control"></input></div>
-            <div className="form-group"><label for="titleInput">Title:</label><input id="titleInput" type="text" class="form-control"></input></div>
-            <div className="form-group"><label for="departmentInput">Department:</label><input id="departmentInput" type="text" class="form-control"></input></div>
-            <div className="form-group"><label for="officeInput">Office:</label><input id="officeInput" type="text" className="form-control"></input></div>
+            <div className="form-group">
+              <label for="nameInput">Name:</label>
+              <input id="nameInput" type="text" class="form-control" />
+            </div>
+            <div className="form-group">
+              <label for="titleInput">Title:</label>
+              <input id="titleInput" type="text" class="form-control" />
+            </div>
+            <div className="form-group">
+              <label for="departmentInput">Department:</label>
+              <input id="departmentInput" type="text" class="form-control" />
+            </div>
+            <div className="form-group">
+              <label for="officeInput">Office:</label>
+              <input id="officeInput" type="text" className="form-control" />
+            </div>
           </div>
           <div className="card-footer">
             <button className="btn btn-danger form-control">Submit</button>
